547e7991deb14d5fa162fb3b417c23d3d296590e
knt529 <kanatin529@gmail.com>
2022-04-23 22:19:12 +0900

    Topにレスポンシブ用の画像を用意。

--- src/tsx/views/components/block/Top.jsx
+++ src/tsx/views/components/block/Top.jsx
@@ -1,12 +1,14 @@
 import "../../../styles/block/Top.css"
 
 import topview from "../../../../img/top-view2.jpg"
+import topview2 from "../../../../img/top-view3.jpg"
 
 function Top(){
   return(
     <div className="top">
       <div className="top-view">
-       <img src={topview}></img>
+       <img className="top-view1" src={topview}></img>
+       <img className="top-view2" src={topview2}></img>
       </div>
       <div className='msg' id="about">
      <h1>Welcome <br className="br"></br>to<br className="br"></br> Kanato's Site !</h1>
